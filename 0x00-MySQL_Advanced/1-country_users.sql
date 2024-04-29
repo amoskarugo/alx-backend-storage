2573e8b3dcfae9bf4859ecbc2a9d1ee6756941ac
amoskarugo <karugoamos@gmail.com>
2024-04-29 19:36:01 +0300

    enum

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- script that creates a table users
+
+CREATE TABLE IF NOT EXISTS  users(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN')  DEFAULT 'US' NOT NULL
+)
